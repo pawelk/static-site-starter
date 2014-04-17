6dc482007d3e8382d21d38d73162c42bf50fd47b
Grzegorz Nyga <nyga.grzegorz@gmail.com>
2014-04-17 23:18:20 +0200

    connect + livereload

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,36 @@ var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var prefix = require('gulp-autoprefixer');
 var preprocess = require('gulp-preprocess');
+var connect = require('gulp-connect');
+
+gulp.task('server', function() {
+	connect.server({
+		root: './compiled/',
+		port: 8080,
+		open: { browser:'Goole Chrome' },
+		livereload: true
+	});
+});
 
 gulp.task('styles', function () {
-    gulp.src('./components/styles/*.styl')
-        .pipe(stylus())
-        .pipe(prefix())
-        .pipe(gulp.dest('./compiled/css/'));
+	gulp.src('./components/styles/*.styl')
+	.pipe(stylus())
+	.pipe(prefix())
+	.pipe(gulp.dest('./compiled/css/'))
+	.pipe(connect.reload());
 });
 
 gulp.task('html', function() {
-  gulp.src('./components/html/*.html')
-    .pipe(preprocess())
-    .pipe(gulp.dest('./compiled/'))
+	gulp.src('./components/html/*.html')
+	.pipe(preprocess())
+	.pipe(gulp.dest('./compiled/'))
+	.pipe(connect.reload());
 });
 
 // Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('./components/styles/*.styl', ['styles']);
-     gulp.watch('./components/html/**/*.html', ['html']);
+gulp.task('watch', ['server'], function() {
+	gulp.watch('./components/styles/*.styl', ['styles']);
+	gulp.watch('./components/html/**/*.html', ['html']);
 });
 
 gulp.task('default', ['html', 'styles', 'watch']);
